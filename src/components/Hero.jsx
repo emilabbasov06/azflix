2b7ee26502caa70974cbd6a25547b805e84c87e2
emilabbasov06 <emilabb13@gmail.com>
2025-01-26 17:57:41 +0400

    worked on navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Hero = () => {
   return (
-    <div style={{ overflow: 'hidden' }} className='w-full flex items-center justify-center min-h-screen'>
-      <div className='container bg-gray-900 opacity-90 border rounded-3xl flex flex-col gap-8 border-gray-700 text-white px-6 py-10 sm:px-10 sm:py-14 lg:px-16 lg:py-20 max-w-6xl'>
-        <h2 className='text-4xl sm:text-5xl lg:text-7xl font-bold text-center'>
-          <span className='text-red-700'>AZF</span>lix.az-a Xoş Gəlmisiniz!
-        </h2>
-        <p className='text-base sm:text-lg lg:text-xl text-gray-300 text-center'>
-          AZFlix.az-da film dünyasına səyahət edin! Burada ən son filmlər, məşhur janrlar, aktyor heyəti və rejissorlar haqqında ətraflı məlumat əldə edə bilərsiniz.
-        </p>
-
-        <div className='flex justify-center'>
-          <Link
-            className='moviesBtn bg-red-700 text-white px-6 py-3 rounded-md text-lg font-medium hover:bg-red-800 transition duration-300'
-            to='/movies'
-          >
-            Film axtar
-          </Link>
-        </div>
-      </div>
-    </div>
+    <div>Hero</div>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import Footer from '../components/Footer';
 
 
 const Home = () => {
   return (
-    <div className='home'>
+    <div>
       <Hero />
+      <Footer />
     </div>
   );
 };
